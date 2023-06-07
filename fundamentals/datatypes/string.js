83dfed37abf60518a5287de30c1e9b4660f0043a
Nasir Mustapha <teboy.com@gmail.com>
2023-06-07 11:33:11 +0100

    initial commit

---
+++ fundamentals/datatypes/string.js
@@ -0,0 +1,29 @@
+x = "";
+y = '';
+
+person = "Abdulrahman Yahya";
+
+person2 = "Abdulrahman Umar";
+
+person3 = "Jamil Salis";
+
+person4 = "Khalifa"
+
+console.log(person4.length);
+console.log(person4.at(1));
+console.log(person4.charAt(1));
+console.log(person3.concat(person4));
+console.log(person3.endsWith('a'));
+console.log(person3.endsWith('s'));
+console.log(person3.includes('mils'));
+console.log(person3.indexOf('a'));
+console.log(person3.lastIndexOf('a'));
+console.log(person4.padEnd(13, ' Shehu'));
+console.log(person4.padStart(13, 'Shehu '));
+console.log(person4.repeat(3));
+console.log(person4.replace('a', 'er'));
+console.log(person4.replaceAll('a', 'er'));
+console.log(person4.slice(1, 6));
+console.log(person3.split(' '));
+console.log(person3.startsWith('J'));
+console.log(person3.startsWith('j'));
